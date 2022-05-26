26ccd004cf2956ccb20772f16a27e202bb91c82d
wfk-tokunaga <wktokunaga@gmail.com>
2022-05-26 11:37:22 -0700

    set up interaction with nav bar

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,33 @@
 import React, {useState} from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     // const [stateValue, updateStateValueFn] = useState(initialState);
     const [formState, setFormState] = useState({name: '', email: '', message: ''});
     const { name, email, message } = formState;
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     function handleChange(event) {
-        setFormState({...formState, [event.target.name]: event.target.value})
+        if (event.target.name === 'email') {
+            const isValid = validateEmail(event.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!event.target.value.length) {
+                setErrorMessage(`${event.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        // Only set the form state if there's no errors
+        if(!errorMessage) {
+            setFormState({...formState, [event.target.name]: event.target.value})
+        }
     }
 
     function handleSubmit(event) {
@@ -21,18 +42,23 @@ function ContactForm() {
                 {/* name */}
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue={name} onChange={handleChange}/>
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange}/>
                 </div>
                 {/* email input */}
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" defaultValue={email} onChange={handleChange}/>
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
                 </div>
                 {/* message text area */}
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}/>
                 </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,6 +7,8 @@ function Nav(props) {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
   } = props;
 
   useEffect(() => {
@@ -23,23 +25,28 @@ function Nav(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li className="mx-2">
-            <span>Contact</span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>
+              Contact
+            </span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
+                currentCategory.name === category.name && 'navActive' &&!contactSelected
                 }`}
               key={category.name}
             >
               <span
                 onClick={() => {
-                  setCurrentCategory(category)
+                  setCurrentCategory(category);
+                  // When clicking on a category, we're NOT selected the contact section
+                  // This means, we should set that setter to false to not render the contact section
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
