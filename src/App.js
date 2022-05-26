d6ed03af682aaa292c84ca0f8aec92b3d155d215
wfk-tokunaga <wktokunaga@gmail.com>
2022-05-26 11:05:13 -0700

    created contact form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -22,8 +23,9 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      ></Nav>
+        ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,29 +1,42 @@
-import React, {useEffect} from "react";
+import React, {useState} from "react";
 
 function ContactForm() {
-return (
-    <section>
-        <h1>Contact me</h1>
-        <form id="contact-form">
-            {/* name */}
-            <div>
-                <label htmlFor="name">Name:</label>
-                <input type="text" name="name"/>
-            </div>
-            {/* email input */}
-            <div>
-                <label htmlFor="email">Email address:</label>
-                <input type="email" name="email"/>
-            </div>
-            {/* message text area */}
-            <div>
-                <label htmlFor="message">Message:</label>
-                <input type="text" name="message" rows="5"/>
-            </div>
-            <button type="submit">Submit</button>
-        </form>
-    </section>
-)
+    // const [stateValue, updateStateValueFn] = useState(initialState);
+    const [formState, setFormState] = useState({name: '', email: '', message: ''});
+    const { name, email, message } = formState;
+
+    function handleChange(event) {
+        setFormState({...formState, [event.target.name]: event.target.value})
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formState);
+    }
+
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                {/* name */}
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange}/>
+                </div>
+                {/* email input */}
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange}/>
+                </div>
+                {/* message text area */}
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
 }
 
 export default ContactForm;
